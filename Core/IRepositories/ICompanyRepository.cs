a83769758b77c15a4ef1861434466c6d515789e2
Arif Huseynov <arif.khuseinov@gmail.com>
2022-04-04 17:51:56 +0200

    UserAssignmentToCompany functionality in process

--- Core/IRepositories/ICompanyRepository.cs
+++ Core/IRepositories/ICompanyRepository.cs
@@ -4,6 +4,11 @@ namespace Core.IRepositories
 {
     public interface ICompanyRepository : IGenericRepository<Company>
     {
-
+        Task AssignToCategory(int companyId, int categoryId);
+        Task AssignToCategory(int companyId, string categoryName);
+        Task AssignToCategory(string companyName, string categoryName);
+        Task ChangeCompanyName(int companyId, string companyNewName);
+        Task ChangeCompanyContactDate(int companyId, DateTime newDate);
+        
     }
 }
---
+++ MobilityManagerApi/Controllers/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Core.Domain;
+using DTOs;
+using Enum;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using UserStoreLogic;
+
+namespace MobilityManagerApi.Controllers
+{
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly UnitOfWork _unitOfWork;
+
+
+        public PlayerController(IMapper mapper, VoucherContext vContext)
+        {
+
+            _mapper = mapper;
+            _unitOfWork = new UnitOfWork(vContext);
+
+        }
+
+        [AuthorizeRolesAttribute(Role.SuperAdmin)]
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreatePlayer(PlayerDto playerDto)
+        {
+            var newPlayer = _mapper.Map<PlayerDto, Player>(playerDto);
+            if (newPlayer == null)
+            {
+                throw new NullReferenceException("Object is not mapped, check mapping profile");
+            }
+
+            await _unitOfWork.Player.AddAsync(newPlayer);
+            await _unitOfWork.Complete();
+            return Ok();
+        }
+
+        [AuthorizeRolesAttribute(Role.SuperAdmin)]
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> DeletePlayer(int playerId)
+        { 
+            
+            var deleted = await _unitOfWork.Player.RemoveAsync(playerId);
+            if(!deleted)
+            {
+                throw new NullReferenceException("Player not found");
+            }
+
+            await _unitOfWork.Complete();
+            return Ok();
+        }
+
+
+    }
+}
